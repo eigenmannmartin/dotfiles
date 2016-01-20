60fb0f38e648c24d13228afc91a5d2a5d78029c8
Martin Eigenmann <github@eigenmannmartin.ch>
2016-01-20 16:44:37 +0100

    removes set bg with feh

--- i3/bothScreen.sh
+++ i3/bothScreen.sh
@@ -1,2 +1,2 @@
 xrandr --output eDP1 --mode 1920x1080 --output HDMI1 --auto --left-of eDP1
-feh --bg-fill ~/Pictures/Wallpapers/BG-Heartbeat.jpg
+#feh --bg-fill ~/Pictures/Wallpapers/BG-Heartbeat.jpg
--- i3/extScreen.sh
+++ i3/extScreen.sh
@@ -1,3 +1,3 @@
 xrandr --output HDMI1 --auto
 xrandr --output eDP1 --off
-feh --bg-fill ~/Pictures/Wallpapers/BG-Heartbeat.jpg
+#feh --bg-fill ~/Pictures/Wallpapers/BG-Heartbeat.jpg
